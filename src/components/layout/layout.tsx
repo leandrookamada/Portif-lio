96dd122adbba2de7a948f8d7189abc1f2d5761d1
leandrookamada <leandrolobatokmd@gmail.com>
2025-06-17 16:35:51 -0300

    adicionando elementos da biblioteca

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../header/header";
 import { QuemSou } from "../quemSou/quemSou"; // <-- Primeira letra MAIÚSCULA
+import Folder from "../../pages/projects/Folder";
 
 export function Layout() {
   return (
@@ -8,6 +9,7 @@ export function Layout() {
       <Header />
       <QuemSou /> {/* <-- Também aqui */}
       <Outlet />
+      <Folder />
     </>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { Layout } from "./components/layout/layout";
+import Folder from "./pages/projects/Folder";
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/folder",
+        element: <Folder />,
+      },
+    ],
   },
 ]);
 export { Router };
